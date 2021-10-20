2cd101b5f051c37a9fbc19c4125614a3e0ea10d2
Md Shoaib Mehedi <mdshoaibmehedi@Mds-MacBook-Air.local>
2021-10-20 18:31:06 +0600

    firestore demo data push

--- src/redux/admin/actions/api.js
+++ src/redux/admin/actions/api.js
@@ -2,36 +2,43 @@ import axios from "axios";
 import { createBlogError, createBlogRequest, createBlogSuccess } from "./crudBlogAction";
 import { createCategoryError, createCategoryRequest, createCategorySuccess } from "./crudCategoriesAction";
 
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
 
 export function createBlogPost(blogContent) {
-  console.log('got the content to write ',blogContent);
-    return (dispatch) => {
-      dispatch(createBlogRequest());
-      axios
-        .post('https://jsonplaceholder.typicode.com/todos',blogContent)
-        .then((response) => {
-          console.log('response of post ',response.data);
-          dispatch(createBlogSuccess(blogContent));
-        })
-        .catch((error) => {
-          dispatch(createBlogError(error.message));
-        });
-    };
-  }
+  console.log('got the content to write ', blogContent);
+  return (dispatch) => {
+    dispatch(createBlogRequest());
+    axios
+      .post('https://jsonplaceholder.typicode.com/todos', blogContent)
+      .then((response) => {
+        console.log('response of post ', response.data);
+        dispatch(createBlogSuccess(blogContent));
+      })
+      .catch((error) => {
+        dispatch(createBlogError(error.message));
+      });
+  };
+}
 
 
-  export function createCategories(catObj) {
-    console.log('got the content to write ',catObj);
-      return (dispatch) => {
-        dispatch(createCategoryRequest());
-        axios
-          .post('https://jsonplaceholder.typicode.com/todos',catObj)
-          .then((response) => {
-            console.log('response of post ',response.data);
-            dispatch(createCategorySuccess(catObj));
-          })
-          .catch((error) => {
-            dispatch(createCategoryError(error.message));
-          });
-      };
+export function createCategories(catObj) {
+  console.log('got the content to write ', catObj);
+  return async (dispatch) => {
+    dispatch(createCategoryRequest());
+
+    const db = getFirestore();
+
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Alan",
+        middle: "Mathison",
+        last: "Turing",
+        born: 1912
+      });
+      dispatch(createCategorySuccess(docRef));
+    } catch (e) {
+      dispatch(createCategoryError(e));
     }
+  };
+}
